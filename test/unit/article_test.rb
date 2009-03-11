dbe2307335baa6fe99c8915a0d49bd4a32ded861
Keith McDonnell <keith@dancingtext.com>
2009-03-11 12:15:24 +0100

    Storing Article.title OK

---
+++ test/unit/article_test.rb
@@ -0,0 +1,25 @@
+require 'test_helper'
+
+class ArticleTest < ActiveSupport::TestCase
+  TITLE = 'Syntax sugar for test unit'
+  
+  def setup
+    @test_unit_syntax_sugar = Article.create(:title => TITLE)
+  end
+  
+  test "should have a title" do
+    assert_equal TITLE, @test_unit_syntax_sugar.title
+  end
+  
+  test "should NOT have an empty title" do
+    assert_raise ActiveRecord::RecordInvalid do
+      @test_unit_syntax_sugar.update_attributes! :title => ''
+    end
+  end
+  
+  test "should NOT have duplicate titles" do
+    assert_raise ActiveRecord::RecordInvalid do
+      Article.create!(:title => TITLE)
+    end
+  end
+end
