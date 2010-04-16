5b5528186f14cd1fdd8fbd06a6a80b5680ce9aab
Keith McDonnell <keith@dancingtext.com>
2010-04-16 15:33:27 +0100

    Also cache homepage content

--- lib/blog.rb
+++ lib/blog.rb
@@ -7,14 +7,14 @@ require 'lib/article'
 
 class Blog < Sinatra::Application
   set :root, APP_ROOT
-
+  before { cache_content }
+  
   get '/' do
     @articles = Article.all
     erb :index
   end
   
   get /^\/(\w+.*)\.html$/ do |title|
-    response.headers['Cache-Control'] = 'public, max-age=44640' # 31 days
     @article = Article.new title
     erb :show
   end
@@ -23,5 +23,11 @@ class Blog < Sinatra::Application
     def email_address
       '&#107;&#101;&#105;&#116;&#104;&#64;&#100;&#97;&#110;&#99;&#105;&#110;&#103;&#116;&#101;&#120;&#116;&#46;&#99;&#111;&#109;'
     end
+  end
+  
+  private
+  
+  def cache_content(seconds=44640)
+    response.headers['Cache-Control'] = "public, max-age=#{seconds}"
   end
 end
